91471537a4fac383b3965717dc5b1e26044d8450
Chris Lambe <chris@chrislambe.com>
2013-05-06 14:20:17 -0500

    Prettied things up a bit.

--- javascripts/presentation.js
+++ javascripts/presentation.js
@@ -4,6 +4,7 @@
       var $element = $(element);
       $element.attr('data-y',500 * (i + 1));
       $element.attr('data-x',2000);
+      $element.attr('data-rotate-y',-90 * i);
     });
 
     impress().init();
