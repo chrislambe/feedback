c2034af7179898cb6daace8baca3fc526facbf2d
Chris Lambe <chris@chrislambe.com>
2013-05-06 18:35:53 -0500

    Switched to SendGrid for SMTP mail delivery.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,11 +52,13 @@ Feedback::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtpcorp.com",
-    port: 2525,
-    user_name: "tech.talks.feedback",
-    password: "",
-    authentication: :login
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
   # Enable threaded mode
