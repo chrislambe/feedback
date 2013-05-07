6662faeb34ac8ce53920de2d370010d4f1f9accb
Chris Lambe <chris@chrislambe.com>
2013-05-07 18:45:29 -0500

    Tweaked layout and graphics a little.

--- javascripts/presentation.js
+++ javascripts/presentation.js
@@ -3,8 +3,11 @@
     $('.app-step').each(function(i,element) {
       var $element = $(element);
       $element.attr('data-y',500 * (i + 1));
-      $element.attr('data-x',2000);
-      $element.attr('data-rotate-y',-90 * i);
+      var offsetX = (i % 4) == 0 ? 0 : ((i % 4) * 500) - 1000;
+      $element.attr('data-x',2000 + offsetX);
+      var offsetZ = i % 2 == 1 ? 500 : (i % 4) * 500;
+      $element.attr('data-z',offsetZ);
+      $element.attr('data-rotate-y',90 * i);
     });
 
     impress().init();
